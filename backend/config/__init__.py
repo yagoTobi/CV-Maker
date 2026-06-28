c8297c48d51d15c1dc7a444bf499c75c3d743016
YagoTobioSouto <201802168@alu.comillas.edu>
2026-06-28 19:18:10 +0100

    chore(assist): add groq dep, assist config, env example

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -1,3 +1,4 @@
 from .settings import settings, validate_production_settings
+from .assist_settings import assist_settings
 
-__all__ = ["settings", "validate_production_settings"]
+__all__ = ["settings", "validate_production_settings", "assist_settings"]
